5579e66940783c0dc48e7b26b3627e9cbb880c15
Spencer Gerritsen <spencergerritsen@gmail.com>
2022-12-22 19:18:18 -0700

    footer start

---
+++ client/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+                <div className="section-body">
+                    <ul className="horizontal list">
+                        <li><a href="https://github.com/icunningham7/">Ian</a></li>
+                        <li><a href="https://github.com/sppencerr/">Spencer</a></li>
+                        <li><a href="https://github.com/nookworth/">Christopher</a></li>
+                        <li><a href="https://github.com/aBraveNewURL/">Ben</a></li>
+
+                    </ul>
+                </div>
+        </footer>
+    )
+};
+
+export default Footer;
