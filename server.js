af9a01ed0b87b1eef2b598919514ac374b045ae6
Benjamin Sokolik <sokolik_b@hotmail.com>
2022-12-26 19:51:26 -0700

    added JWT info to package.json and server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const app = express();
 const connectToMongoDB = require('./config/connection');
+const jwt = require('jsonwebtoken');
 
 
 // Connect to the MongoDB database
@@ -41,6 +43,51 @@ app.use('/graphql', graphqlHTTP({
   rootValue: // your root resolver object
   graphiql: true // enable the GraphiQL interface
 }));
+
+// START: added portion with JWT components
+app.use(express.json())
+
+// TODO posts can be commented out or removed completely once tested 
+const posts = [
+    {
+        username: 'TEST1',
+        title: 'First post'
+    },
+    {
+        username: 'TEST2',
+        title: 'Second post'
+    },
+    {
+        username: 'TEST3',
+        title: 'Third post'
+    }
+]
+
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post = post.username === req.user.name))
+})
+
+app.post('/login', (req, res) => {
+    // user will need authentication - bcrypt? 
+    const username = req.body.username
+    const user = { name: username }
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({ accessToken: accessToken })
+})
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null)
+        return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
+// END: added portion with JWT components
 
 app.listen(4000, () => {
   console.log('Server listening on port 4000');
