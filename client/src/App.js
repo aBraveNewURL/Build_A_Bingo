f300d6be3ad69c31a6b0e9ed021818ad970c1874
Benjamin Sokolik <sokolik_b@hotmail.com>
2022-12-27 18:27:47 -0700

    created Bingo dir, Bingo.js, Bingo.css, updated App.js, fixed server.js bugs

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import './Bingo/Bingo'
+import Game from './Bingo/Bingo';
+import './Bingo/Bingo.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game/>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -8,88 +8,89 @@ const jwt = require('jsonwebtoken');
 
 // Connect to the MongoDB database
 connectToMongoDB();
-  // Create a new user account
-  app.post('/api/users', (req, res) => {
-    // Get the user data from the request body
-    const user = req.body;
+// Create a new user account
+app.post('/api/users', (req, res) => {
+  // Get the user data from the request body
+  const user = req.body;
 
-    // Hash the password
-    user.password = hashPassword(user.password);
+  // Hash the password
+  user.password = hashPassword(user.password);
 
-    // Insert the user into the Users collection
-    db.collection('Users').insertOne(user, (err, result) => {
-      if (err) {
-        console.error(err);
-        res.sendStatus(500);
+  // Insert the user into the Users collection
+  db.collection('Users').insertOne(user, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
 
-// create operation using User.create method
-app.post('/users', function(req, res) {
-  const user = new User({
-    username: req.body.username,
-    password: req.body.password
-  });
-  user.save(function(error) {
-    if (error) {
-      res.status(500).send(error);
-    } else {
-      res.send(user);
+      // create operation using User.create method
+      app.post('/users', function (req, res) {
+        const user = new User({
+          username: req.body.username,
+          password: req.body.password
+        });
+        user.save(function (error) {
+          if (error) {
+            res.status(500).send(error);
+          } else {
+            res.send(user);
+          }
+        });
+      })
     }
-  });
+  })
 });
 
+  app.use('/graphql', graphqlHTTP({
+    schema: '',// your GraphQL schema
+    rootValue: '',// your root resolver object
+    graphiql: true // enable the GraphiQL interface
+  }));
 
-app.use('/graphql', graphqlHTTP({
-  schema: // your GraphQL schema
-  rootValue: // your root resolver object
-  graphiql: true // enable the GraphiQL interface
-}));
+  // START: added portion with JWT components
+  app.use(express.json())
 
-// START: added portion with JWT components
-app.use(express.json())
-
-// TODO posts can be commented out or removed completely once tested 
-const posts = [
+  // TODO 'posts' variable can be commented out or removed completely once tested 
+  const posts = [
     {
-        username: 'TEST1',
-        title: 'First post'
+      username: 'TEST1',
+      title: 'First post'
     },
     {
-        username: 'TEST2',
-        title: 'Second post'
+      username: 'TEST2',
+      title: 'Second post'
     },
     {
-        username: 'TEST3',
-        title: 'Third post'
+      username: 'TEST3',
+      title: 'Third post'
     }
-]
+  ]
 
-app.get('/posts', authenticateToken, (req, res) => {
+  app.get('/posts', authenticateToken, (req, res) => {
     res.json(posts.filter(post = post.username === req.user.name))
-})
+  })
 
-app.post('/login', (req, res) => {
+  app.post('/login', (req, res) => {
     // user will need authentication - bcrypt? 
     const username = req.body.username
     const user = { name: username }
     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
     res.json({ accessToken: accessToken })
-})
+  })
 
-function authenticateToken(req, res, next) {
+  function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
     if (token == null)
-        return res.sendStatus(401)
+      return res.sendStatus(401)
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403)
-        req.user = user
-        next()
+      if (err) return res.sendStatus(403)
+      req.user = user
+      next()
     })
-}
-// END: added portion with JWT components
+  }
+  // END: added portion with JWT components
 
-app.listen(4000, () => {
-  console.log('Server listening on port 4000');
-});
-
+  app.listen(4000, () => {
+    console.log('Server listening on port 4000');
+  });
