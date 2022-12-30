b4637f47047619d17f94997548531f2a3cef13c9
Ian Cunningham <35973428+icunningham7@users.noreply.github.com>
2022-12-30 12:24:20 -0700

    add bingolist and bingocard generators

--- server/seeds/data.js
+++ server/seeds/data.js
@@ -25,9 +25,125 @@ const emailList = [
     
 ];
 
+const bingoList = [
+    {
+        "name": "Desserts",
+        "list": [
+            "Ice cream",
+            "Donuts",
+            "Milkshakes",
+            "Cake",
+            "Pie",
+            "Flan",
+            "Mousse",
+            "Creme brulee",
+            "Pot de creme",
+            "Pudding",
+            "Cookies",
+            "Brownies",
+            "Brookies",
+            "Pazookies",
+            "Creme fraiche",
+            "Bananas foster",
+            "Strawberries and cream",
+            "Fruit compote",
+            "Baked apples",
+            "Ganache",
+            "Chocolate fondue",
+            "Chocolate drizzle",
+            "Chocolate nuts",
+            "Chocolate chips"
+        ]
+    },
+    {
+        "name": "TV Shows",
+        "list": [
+            "Castlevania",
+            "Gravity Falls",
+            "The Last Kingdom",
+            "Vikings",
+            "Vinland Saga",
+            "Kakegurui",
+            "3%",
+            "She-Ra",
+            "Wednesday",
+            "Andor",
+            "The Mandalorian",
+            "Ozark",
+            "Dahmer",
+            "The Boys",
+            "Demon Slayer",
+            "Hunter x Hunter",
+            "Samurai Seven",
+            "All My Children",
+            "Married with Children",
+            "Cheers",
+            "Friends",
+            "Fawlty Towers",
+            "The IT Crowd",
+            "The Great British Bake-Off"
+        ]
+    },
+    {
+        "name": "Video Games",
+        "list": [
+            "Elden Ring",
+            "Nuclear Throne",
+            "Human Resource Machine",
+            "The Outer Wilds",
+            "Dota 2",
+            "League of Legends",
+            "Call of Duty",
+            "Overwatch",
+            "Jump King",
+            "Dark Souls",
+            "Dead Cells",
+            "Bloodstained: Ritual of the Night",
+            "Dead by Daylight",
+            "Alien: Isolation",
+            "Spelunky",
+            "Samurai Gunn",
+            "Demons with Shotguns",
+            "Nine Parchments",
+            "Magicka",
+            "Terraria",
+            "Minecraft",
+            "Rim World",
+            "Tales of Maj'Eyal",
+            "Cult of the Lamb"
+        ]
+    }
+];
+
 const getRandomIndex = (arr) => Math.floor(Math.random() * arr.length);
 const getRandomIndexValue = (arr) => arr[Math.floor(Math.random() * arr.length)];
-const getRandomBingoNumber = (num = 100) => Math.floor(Math.random() * num.length);
+
+const getRandomBingoNumber = (num = 100) => Math.floor(Math.random() * num);
+
+const getRandomBingoNumberList = () => {
+    const name = 'Random Number BingoList';
+    const list = [];
+
+    while ( list.length < 24 ) {
+        const num = getRandomBingoNumber();
+        list.push(num);
+    };
+
+    const bingoList = {
+        name: name,
+        list: list
+    };
+
+    return bingoList;
+}
+
+const getDummyBingoNumberCard = () => {
+    const col = ['a', 'b', 'c', 'd', 'e'];
+    const row = ['1', '2', '3', '4', '5'];
+
+
+}
+
 const getRandomDummyUsers = (usernameArr, emailArr, count) => {
     const uniqueAccountLimit = Math.min(usernameArr.length, emailArr.length);
     const dummyUsers = [];
@@ -58,8 +174,10 @@ const getRandomDummyUsers = (usernameArr, emailArr, count) => {
 module.exports = {
     usernameList,
     emailList,
+    bingoList,
     getRandomIndex,
     getRandomIndexValue,
     getRandomBingoNumber,
+    getRandomBingoNumberList,
     getRandomDummyUsers
 };
--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,17 +1,37 @@
 const db = require('../config/connection');
-const { User } = require('../models');
-const { emailList, usernameList, getRandomIndex, getRandomIndexValue, getRandomDummyUsers, getRandomBingoNumber } = require('./data');
-
-console.log('usernameList:', usernameList);
-console.log('emailList:', emailList);
+const { User, BingoList } = require('../models');
+const { emailList, usernameList, bingoList, getRandomIndex, getRandomIndexValue, getRandomDummyUsers, getRandomBingoNumber, getRandomBingoNumberList } = require('./data');
 
 db.once('open', async () => {
 
-    await User.deleteMany({});
+    const numList = getRandomBingoNumberList();
 
-    const dummyUsers = getRandomDummyUsers(usernameList, emailList, 10);
+    console.log(numList);
+    console.log(numList.list.length);
 
-    await User.collection.insertMany(dummyUsers);
+    // await User.deleteMany({});
+    // await BingoList.deleteMany({});
+    
+    // // create users list, then add them to the db
+    // const dummyUserList = getRandomDummyUsers(usernameList, emailList, 10);
+    // const users = await User.collection.insertMany(dummyUserList);
+    
+
+    // const userForNewLists = await User.findOne({}).select('_id').exec();
+
+    // const bingoListData = await bingoList.map((list) => {
+    //     list.owner = userForNewLists;
+    //     return list;
+    // });
+
+    // await BingoList.collection.insertMany(bingoListData);
+
+    // const manyUsers = await User.find({}).select('_id').limit(10);
+    
+    // manyUsers.map((user) => {
+    //     console.log(user);
+
+    // })
     process.exit(0);
 
 });
