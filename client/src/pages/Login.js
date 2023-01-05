e2974ebfb1d5a2a5813a08f3898cb3afc3dcc0f4
Spencer Gerritsen <spencergerritsen@gmail.com>
2023-01-04 21:32:03 -0700

    styling login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -30,42 +30,56 @@ function Login(props) {
   };
 
   return (
-    <div className="container my-1">
-      <Link to="/signup">← Go to Signup</Link>
+    // <div className="container my-1">
+    //   <Link to="/signup">← Go to Signup</Link>
+    <div class="text-xl font-semibold mt-2 pt-1 mb-0">
+    Don't have an account?
+    <Link to="/signup" className="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out">
+Register
+</Link>
+    
 
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        {error ? (
-          <div>
-            <p className="error-text">The provided credentials are incorrect</p>
-          </div>
-        ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+<h2 className="font-semibold text-2xl text-center my-4">Login</h2>
+<form className='mx-auto max-w-sm' onSubmit={handleFormSubmit}>
+  <div className="mb-6 flex-row">
+    <label htmlFor="email">Email address:</label>
+    <input
+      placeholder="youremail@test.com"
+      name="email"
+      type="email"
+      id="email"
+      className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+      onChange={handleChange}
+    />
+  </div>
+  <div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
+    <p className="text-center text-white font-semibold mx-4 mb-0">and</p>
+  </div>
+  <div className="mb-6">
+    <label htmlFor="pwd">Password:</label>
+    <input
+      placeholder="******"
+      name="password"
+      type="password"
+      id="pwd"
+      className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+      onChange={handleChange}
+    />
+  </div>
+  {error ? (
+    <div>
+      <p className="error-text">The provided credentials are incorrect</p>
     </div>
+  ) : null}
+  <div className="flex-row flex-end">
+    <button className='' type="submit">Login</button>
+  </div>
+</form>
+</div>
   );
 }
 
 export default Login;
+
+
+
